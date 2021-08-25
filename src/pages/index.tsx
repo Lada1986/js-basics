63547ace3eabefcda5d982819b2df484a791eb42
Lada Tomasek <ladatomasek86@gmail.com>
2021-08-25 03:16:26 +0200

    new project updates

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * react from 'react';
+
+class Index extends React.Component<()> {
+    render () {
+        return (
+            <div>Hello World</div>
+        )
+    }
+}
+
+export deafult Index;
